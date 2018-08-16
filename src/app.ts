8fb3fd1ecd8f639d42d9373674dee8a4f6c3e9ba
cindy-alvarado <38857997+cindy-alvarado@users.noreply.github.com>
2018-08-16 04:55:21 -0400

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log('Hello TypeScript!');
+const pizzas = [
+    { name: 'Pepperoni', toppings: ['pepperoni'] }
+];
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const pizza = {
+    name: 'Blazing Infrno',
+    getName: function() {
+        setTimeout(() => {
+            console.log(this);
+        }, 100);
+    },
+};
+
+console.log(pizza.getName());
